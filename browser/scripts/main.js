1990adfb5ea965514144fe5c09ae945ac7cc1433
Jacob Carpenter <jacobcarpenter@gmail.com>
2015-11-15 13:56:51 -0800

    Add ship.

--- browser/scripts/main.js
+++ browser/scripts/main.js
@@ -1,1 +1,24 @@
-alert('hello!');
+import { Phaser } from 'phaser';
+
+const width = 480;
+const height = 300;
+
+const shipHeight = 40;
+const shipWidth = 120;
+
+const game = new Phaser.Game(width, height, Phaser.CANVAS, '', {
+	create,
+	update
+});
+
+let ship;
+
+function create() {
+	ship = game.add.graphics(20, height / 2 - shipHeight / 2);
+	ship.beginFill(0xFFFFFF, 0xFF);
+	ship.drawRect(0, 0, shipWidth, shipHeight);
+}
+
+function update() {
+
+}

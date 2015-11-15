736dd7966221bb17817bebfa9f8825ccfc3ced1d
Jacob Carpenter <jacobcarpenter@gmail.com>
2015-11-15 13:25:00 -0800

    Add phaser.

--- browser/config.js
+++ browser/config.js
@@ -17,6 +17,7 @@ System.config({
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "core-js": "npm:core-js@1.2.6",
+    "phaser": "github:photonstorm/phaser@2.4.4",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
